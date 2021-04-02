e505937ff8847de5c34c40bd4c0636e7226b8307
DIVYANSHU VASHISHT <67421562+DivyanshuVashisht@users.noreply.github.com>
2021-04-02 21:00:31 +0530

    Update and rename Exp2Histest.m to HistogramSmoothing.m

--- HistogramSmoothing.m
+++ HistogramSmoothing.m
@@ -1,14 +1,12 @@
-%NAME: DIVYANSHU VASHISHT
-%REG NO: 19BCN7234
-%DATE: 27 FEB 2021
-%LAB L57-L58
-clc;
-clear all;
+
 % Gaussian filter - used for the smoothening purposes of the image
 % different from histogram distribution
 % nature of gaussian filter - try to make the image blur because image is
 % one type of low pass filter(high frequency to low frequency i.e. very
 % high change to very low change)
+clc;
+clear all;
+
 I = imread('TestImage1.jpg');
 I2=imread('TestImage.jpg');
 I2=I2(:,:,1);% if img is not in grayscale
